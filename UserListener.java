b5c6e86ffbe69d23ff5264aa44c7c829f94fa1a9
Jhonattan Rodriguez <jjrodriguezn@unal.edu.co>
2014-04-29 12:49:24 -0500

    Update UserListener.java

--- UserListener.java
+++ UserListener.java
@@ -1,1 +1,34 @@
-kk
+import java.util.ArrayList;
+
+import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+class UserListener extends UntypedActor
+{
+
+	LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+
+	@Override
+	public void onReceive(Object message) throws Exception 
+	{
+		sumStuff = (boolean)message; 
+		while(sumStuff)
+		{
+			if(modify.size() > 0)
+			{
+				System.out.println("Hey "+modify.get(0));
+				Banco.modifyBalance(modify.get(0));
+				modify.remove(0);
+			}
+		}
+	}	
+	
+	public static void addStuff(Integer i)
+	{
+		modify.add(i);
+	}
+	
+	boolean sumStuff = false;
+	private static ArrayList<Integer> modify = new ArrayList<>();
+}
